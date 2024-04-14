4bf6045bd14b20fd76b8108ebd6067b9c78c7589
JoannaCi <joanna.cicha001@gmail.com>
2024-04-14 15:13:13 +0200

    fix: to much commented in previous commit

--- su/su-4/su.py
+++ su/su-4/su.py
@@ -6,42 +6,41 @@ x_axis = dane[:, :-1]
 y_axis = dane[:, -1]
 print("Wymiar x", x_axis.shape, "Wymair y", y_axis.shape)
 
-# Liczba atrybutów
 num_attributes = dane.shape[1]
 print("Liczba atrybutów:", num_attributes)
 
-# # Plot
-# plt.figure(figsize=(12, 8))
-# bp = plt.boxplot(x_axis)
+# Plot
+plt.figure(figsize=(12, 8))
+bp = plt.boxplot(x_axis)
 
-# num_boxes = len(bp['boxes'])
-# colors = plt.cm.rainbow(np.linspace(0, 1, num_boxes))
+num_boxes = len(bp['boxes'])
+colors = plt.cm.rainbow(np.linspace(0, 1, num_boxes))
 
-# for box, color in zip(bp['boxes'], colors):
-#     box.set(color=color, linewidth=2)
+for box, color in zip(bp['boxes'], colors):
+    box.set(color=color, linewidth=2)
 
-# plt.xticks(np.arange(1, x_axis.shape[1] + 1), rotation=90)
-# plt.xlabel('Features', fontsize=14)
-# plt.ylabel('Values', fontsize=14)
-# plt.title('Boxplot of Features', fontsize=16)
-# plt.grid(True, linestyle='--', alpha=0.7)
-# plt.show()
+plt.xticks(np.arange(1, x_axis.shape[1] + 1), rotation=90)
+plt.xlabel('Features', fontsize=14)
+plt.ylabel('Values', fontsize=14)
+plt.title('Boxplot of Features', fontsize=16)
+plt.grid(True, linestyle='--', alpha=0.7)
+plt.show()
 
-# # Histogram
-# plt.figure(figsize=(8, 6))
-# counts, bins, _ = plt.hist(x_axis, bins=20, edgecolor='black', alpha=0.7)
+# Histogram
+plt.figure(figsize=(8, 6))
+counts, bins, _ = plt.hist(x_axis, bins=20, edgecolor='black', alpha=0.7)
 
-# colors = np.random.rand(len(counts))  # Random colours
+colors = np.random.rand(len(counts))  # Random colours
 
-# for i in range(len(bins) - 1):  # Iterate up to len(bins) - 1, so there won't be index out of range
-#     plt.bar(bins[i], counts[i], width=bins[i + 1] - bins[i], color=plt.cm.viridis(colors[i]), edgecolor='black', alpha=0.7)
+for i in range(len(bins) - 1):  # Iterate up to len(bins) - 1, so there won't be index out of range
+    plt.bar(bins[i], counts[i], width=bins[i + 1] - bins[i], color=plt.cm.viridis(colors[i]), edgecolor='black', alpha=0.7)
 
-# plt.xlabel('Class', fontsize=14)
-# plt.ylabel('Frequency', fontsize=14)
-# plt.title('Distribution of Class', fontsize=16)
+plt.xlabel('Class', fontsize=14)
+plt.ylabel('Frequency', fontsize=14)
+plt.title('Distribution of Class', fontsize=16)
 
-# plt.grid(True, linestyle='--', alpha=0.7)
-# plt.show()
+plt.grid(True, linestyle='--', alpha=0.7)
+plt.show()
 
 correlation_matrix = np.corrcoef(dane, rowvar=False)
 
