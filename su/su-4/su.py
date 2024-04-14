eaa1c4182ae6f3772a7b092022cffa0c9c5ef7c2
JoannaCi <joanna.cicha001@gmail.com>
2024-04-14 16:17:34 +0200

    fix: removed duplicated code and fixed language inconsistencies

--- su/su-4/su.py
+++ su/su-4/su.py
@@ -9,10 +9,10 @@ from sklearn.pipeline import make_pipeline
 dane = np.genfromtxt("147963-regression.txt", skip_header=1)
 x_axis = dane[:, :-1]
 y_axis = dane[:, -1]
-print("Wymiar x", x_axis.shape, "Wymiar y", y_axis.shape)
+print("X shape:", x_axis.shape, "Y shape:", y_axis.shape)
 
 num_attributes = dane.shape[1]
-print("Liczba atrybutów:", num_attributes)
+print("Number of attributes:", num_attributes)
 
 # Plot
 plt.figure(figsize=(12, 8))
@@ -63,8 +63,8 @@ low_corr_pairs = np.where(np.abs(correlation_matrix) < low_corr_threshold)
 low_corr_pairs = [(i, j) for i, j in zip(low_corr_pairs[0], low_corr_pairs[1]) if i != j]
 num_low_corr_pairs = len(low_corr_pairs)
 
-print("Liczba par atrybutów z wysoką korelacją:", num_high_corr_pairs)
-print("Liczba par atrybutów z niską korelacją:", num_low_corr_pairs)
+print("Number of attribute pairs with high correlation:", num_high_corr_pairs)
+print("Number of attribute pairs with low correlation:", num_low_corr_pairs)
 
 # Definition of regresors
 regresors = {
@@ -75,77 +75,84 @@ regresors = {
     "SVR Linear Kernel": svm.SVR(kernel='linear'),
     "SVR RBF Kernel": svm.SVR(kernel='rbf')
 }
-# Calculating the difference between the values of the metrics before and after standardisation
+
+# Function to calculate the difference between the values of the metrics before and after standardisation
 def calculate_difference(metric, before, after):
     difference = {}
     for name in before.keys():
         difference[name] = after[name] - before[name]
     return difference
 
-results_mae_no_scaling = {}
-results_r2_no_scaling = {}
+# Function to perform calculations and generate plots
+def calculate_and_plot(regresors, x_axis, y_axis):
+    # Calculation of metrics results without scaling
+    results_mae_no_scaling = {}
+    results_r2_no_scaling = {}
 
-for name, regresor in regresors.items():
-    regresor.fit(x_axis, y_axis)
-    y_pred = regresor.predict(x_axis)
-    mae = mean_absolute_error(y_axis, y_pred)
-    r2 = r2_score(y_axis, y_pred)
-    results_mae_no_scaling[name] = mae
-    results_r2_no_scaling[name] = r2
+    for name, regresor in regresors.items():
+        regresor.fit(x_axis, y_axis)
+        y_pred = regresor.predict(x_axis)
+        mae = mean_absolute_error(y_axis, y_pred)
+        r2 = r2_score(y_axis, y_pred)
+        results_mae_no_scaling[name] = mae
+        results_r2_no_scaling[name] = r2
 
-results_mae_with_scaling = {}
-results_r2_with_scaling = {}
+    # Calculation of metrics results with scaling
+    results_mae_with_scaling = {}
+    results_r2_with_scaling = {}
 
-for name, regresor in regresors.items():
-    if name != "DecisionTreeRegressor":  # The decision tree does not need to be standardised
-        regresor = make_pipeline(StandardScaler(), regresor)
-    regresor.fit(x_axis, y_axis)
-    y_pred = regresor.predict(x_axis)
-    mae = mean_absolute_error(y_axis, y_pred)
-    r2 = r2_score(y_axis, y_pred)
-    results_mae_with_scaling[name] = mae
-    results_r2_with_scaling[name] = r2
+    for name, regresor in regresors.items():
+        if name != "DecisionTreeRegressor":  # The decision tree does not need to be standardised
+            regresor = make_pipeline(StandardScaler(), regresor)
+        regresor.fit(x_axis, y_axis)
+        y_pred = regresor.predict(x_axis)
+        mae = mean_absolute_error(y_axis, y_pred)
+        r2 = r2_score(y_axis, y_pred)
+        results_mae_with_scaling[name] = mae
+        results_r2_with_scaling[name] = r2
 
-# Comparison of metrics results
-plt.figure(figsize=(12, 6))
+    # Comparison of metrics results
+    plt.figure(figsize=(12, 6))
 
-plt.subplot(1, 2, 1)
-plt.bar(range(len(results_mae_no_scaling)), list(results_mae_no_scaling.values()), align='center', alpha=0.5, label='Without Scaling')
-plt.bar(range(len(results_mae_with_scaling)), list(results_mae_with_scaling.values()), align='center', alpha=0.5, label='With Scaling')
-plt.xticks(range(len(regresors)), list(regresors.keys()), rotation=45)
-plt.title('Mean Absolute Error')
-plt.ylabel('MAE')
-plt.legend()
+    plt.subplot(1, 2, 1)
+    plt.bar(range(len(results_mae_no_scaling)), list(results_mae_no_scaling.values()), align='center', alpha=0.5, label='Without Scaling')
+    plt.bar(range(len(results_mae_with_scaling)), list(results_mae_with_scaling.values()), align='center', alpha=0.5, label='With Scaling')
+    plt.xticks(range(len(regresors)), list(regresors.keys()), rotation=45)
+    plt.title('Mean Absolute Error')
+    plt.ylabel('MAE')
+    plt.legend()
 
-plt.subplot(1, 2, 2)
-plt.bar(range(len(results_r2_no_scaling)), list(results_r2_no_scaling.values()), align='center', alpha=0.5, label='Without Scaling')
-plt.bar(range(len(results_r2_with_scaling)), list(results_r2_with_scaling.values()), align='center', alpha=0.5, label='With Scaling')
-plt.xticks(range(len(regresors)), list(regresors.keys()), rotation=45)
-plt.title('R^2 Score')
-plt.ylabel('R^2')
-plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.bar(range(len(results_r2_no_scaling)), list(results_r2_no_scaling.values()), align='center', alpha=0.5, label='Without Scaling')
+    plt.bar(range(len(results_r2_with_scaling)), list(results_r2_with_scaling.values()), align='center', alpha=0.5, label='With Scaling')
+    plt.xticks(range(len(regresors)), list(regresors.keys()), rotation=45)
+    plt.title('R^2 Score')
+    plt.ylabel('R^2')
+    plt.legend()
 
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.show()
 
-# Calculation of differences between values of metrics before and after standardisation
-mae_difference = calculate_difference("Mean Absolute Error", results_mae_no_scaling, results_mae_with_scaling)
-r2_difference = calculate_difference("R^2 Score", results_r2_no_scaling, results_r2_with_scaling)
+    # Calculation of differences between values of metrics before and after standardisation
+    mae_difference = calculate_difference("Mean Absolute Error", results_mae_no_scaling, results_mae_with_scaling)
+    r2_difference = calculate_difference("R^2 Score", results_r2_no_scaling, results_r2_with_scaling)
 
-# Comparison of metrics results
-plt.figure(figsize=(12, 6))
+    # Comparison of metrics results
+    plt.figure(figsize=(12, 6))
 
-plt.subplot(1, 2, 1)
-plt.bar(range(len(mae_difference)), list(mae_difference.values()), align='center', alpha=0.5)
-plt.xticks(range(len(regresors)), list(regresors.keys()), rotation=45)
-plt.title('Mean Absolute Error Difference')
-plt.ylabel('Difference')
+    plt.subplot(1, 2, 1)
+    plt.bar(range(len(mae_difference)), list(mae_difference.values()), align='center', alpha=0.5)
+    plt.xticks(range(len(regresors)), list(regresors.keys()), rotation=45)
+    plt.title('Mean Absolute Error Difference')
+    plt.ylabel('Difference')
 
-plt.subplot(1, 2, 2)
-plt.bar(range(len(r2_difference)), list(r2_difference.values()), align='center', alpha=0.5)
-plt.xticks(range(len(regresors)), list(regresors.keys()), rotation=45)
-plt.title('R^2 Score Difference')
-plt.ylabel('Difference')
+    plt.subplot(1, 2, 2)
+    plt.bar(range(len(r2_difference)), list(r2_difference.values()), align='center', alpha=0.5)
+    plt.xticks(range(len(regresors)), list(regresors.keys()), rotation=45)
+    plt.title('R^2 Score Difference')
+    plt.ylabel('Difference')
 
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.show()
+
+calculate_and_plot(regresors, x_axis, y_axis)
